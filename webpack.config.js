7931b2aef308087da0c093792a75fe2f209f8009
devindurrant303 <devindurrant303@gmail.com>
2017-03-31 13:36:01 -0600

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+	devtool: 'source-map',
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
